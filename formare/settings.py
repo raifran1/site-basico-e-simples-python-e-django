639d12446e3d177abf3936cb550057c35eccc8de
raifran1 <raifranlucas@outlook.com>
2019-12-30 16:16:42 -0300

    linguagem padrao

--- formare/settings.py
+++ formare/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-br' 
 
 TIME_ZONE = 'UTC'
 
