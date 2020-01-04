62e220fe458d02fea871385b69fff6ba167e715b
raifran1 <raifranlucas@outlook.com>
2020-01-04 20:37:37 -0300

    cadastro financeiro, aluno, turmas

--- apps/clientes/admin.py
+++ apps/clientes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Aluno, Turma, Financeiro
 # Register your models here.
 
 @admin.register(Cliente)
@@ -8,3 +8,27 @@ class ClienteAdmin(admin.ModelAdmin):
     search_fields = ('name','cpfCnpj')
     #list_filter = ('status_disponibilidade')
     #list_editable = ('status_disponibilidade',)
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome_aluno', 'serie','turma','financeiro')
+    search_fields = ('nome_aluno','escola',)
+    list_filter = ('serie','turma',)
+    list_editable = ('turma',)
+
+
+@admin.register(Turma)
+class TurmaAdmin(admin.ModelAdmin):
+    list_display = ('nome_turma', 'horario_inicial','horario_final')
+    search_fields = ('nome_turma',)
+    # list_filter = ('serie','turma')
+    list_editable = ('horario_inicial','horario_final')
+
+
+@admin.register(Financeiro)
+class FinanceiroAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'preco_mensalidade','vencimento','matricula', 'feveveiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro', )
+    search_fields = ('cliente',)
+    # list_filter = ('serie','turma')
+    list_editable = ('matricula', 'feveveiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro', )
--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -2,21 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    name = models.CharField(max_length=70)
-    cpfCnpj = models.CharField(max_length=17)
-    email = models.EmailField(max_length=254) 
-    mobilePhone = models.CharField(max_length=50)
-    postalCode = models.IntegerField()
-    addressNumber = models.IntegerField()
-    descricao_alunos = models.TextField()
+    name = models.CharField(verbose_name=u'Nome Completo',max_length=70)
+    cpfCnpj = models.CharField(verbose_name=u'CPF',max_length=17)
+    email = models.EmailField(verbose_name=u'Email',max_length=254,help_text='Utilizar um email válido') 
+    mobilePhone = models.CharField(verbose_name=u'Telefone',max_length=50,help_text='Utilize um número válido')
+    postalCode = models.IntegerField(verbose_name=u'CEP',help_text='Utilize um CEP válido')
+    addressNumber = models.IntegerField(verbose_name=u'Número da Casa',)
+    descricao_alunos = models.TextField(verbose_name=u'Sobre os Alunos',help_text='Campo para utilizar para Cadastro de Aluno')
     
+    class Meta:
+        verbose_name = u'Cliente'
+        verbose_name_plural = u'Clientes'
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
 
 class Aluno(models.Model):
-    nome_aluno = models.CharField(max_length=70)
-    serie = models.CharField(max_length=50,choices=(("1º Ano - Fundamental I", ("1º Ano - Fundamental I")),
+    nome_aluno = models.CharField(verbose_name=u'Nome Completo',max_length=70)
+    serie = models.CharField(verbose_name=u'Série',max_length=50,choices=(("1º Ano - Fundamental I", ("1º Ano - Fundamental I")),
                                              ("2º Ano - Fundamental I", ("2º Ano - Fundamental I")),
                                              ("3º Ano - Fundamental I", ("3º Ano - Fundamental I")),
                                              ("4º Ano - Fundamental I", ("4º Ano - Fundamental I")),
@@ -26,20 +31,21 @@ class Aluno(models.Model):
                                              ("8º Ano - Fundamental II", ("8º Ano - Fundamental II")),
                                              ("9º Ano - Fundamental II", ("9º Ano - Fundamental II")),
                                              ("1º Ano - Médio", ("1º Ano - Médio")),
-                                             ("2º Ano - Médio", ("1º Ano - Médio")),
+                                             ("2º Ano - Médio", ("2º Ano - Médio")),
                                              ),default="")
-    escola = models.CharField(max_length=50)
-    idade = models.IntegerField()
-    necessidade_especial = models.BooleanField(default=False)
-    preco = models.IntegerField()
-    desconto = models.IntegerField(default=0)
-    observacoes = models.TextField(blank=True,null=True)
-    status =  models.CharField(max_length=50,choices=(("Ativo", ("Ativo")),
-                                             ("Atrasado", ("Atrasado")),
-                                             ("Pendente", ("Pendente")),
-                                             ),default="")
-    responsavel = models.ForeignKey("Cliente", on_delete=models.DO_NOTHING)
-    turma = models.ForeignKey("Turma",on_delete=models.DO_NOTHING)
+    escola = models.CharField(verbose_name=u'Escola',max_length=50,blank=True, null=True)
+    idade = models.IntegerField(verbose_name=u'Idade',blank=True, null=True)
+    necessidade_especial = models.BooleanField(verbose_name=u'Necessidade Especial?',default=False)
+    financeiro = models.ForeignKey("Financeiro", verbose_name=u'Cadastro Financeiro', on_delete=models.CASCADE)
+    responsavel = models.ForeignKey("Cliente", verbose_name=u'Responsável',on_delete=models.DO_NOTHING)
+    turma = models.ForeignKey("Turma",verbose_name=u'Turma',on_delete=models.DO_NOTHING)
+    observacoes = models.TextField(verbose_name=u'Observações',blank=True,null=True,help_text='Observações Gerais sobre o Aluno')
+
+    class Meta:
+        verbose_name = u'Aluno'
+        verbose_name_plural = u'Alunos'
+        ordering = ['nome_aluno']
+
 
     def __str__(self):
         return self.nome_aluno    
@@ -52,3 +58,31 @@ class Turma(models.Model):
 
     def __str__(self):
         return self.nome_turma
+
+class Financeiro(models.Model):
+    cliente = models.ForeignKey("Cliente", verbose_name=u'Cliente', on_delete=models.CASCADE)
+    preco_matricula = models.FloatField(u'Preço da Matrícula',)
+    preco_mensalidade = models.FloatField(u'Preço da Mensalidade',)
+    vencimento = models.IntegerField(u'Vencimento',)
+    matricula = models.BooleanField(u'MATR.',)
+    # Jan = models.BooleanField(u'Matr.',)
+    feveveiro = models.BooleanField(u'FEV.',)
+    marco = models.BooleanField(u'MAR.',)
+    abril = models.BooleanField(u'ABR.',)
+    maio = models.BooleanField(u'MAI.',)
+    junho = models.BooleanField(u'JUN.',)
+    julho = models.BooleanField(u'JUL.',)
+    agosto = models.BooleanField(u'AGO.',)
+    setembro = models.BooleanField(u'SET.',)
+    outubro = models.BooleanField(u'OUT.',)
+    novembro = models.BooleanField(u'NOV.',)
+    dezembro = models.BooleanField(u'DEZ.',)
+
+    class Meta:
+        verbose_name = u'Cadastro Financeiro'
+        verbose_name_plural = u'Cadastro Financeiro'
+        ordering = ['cliente']
+
+    def __str__(self):
+        return u'{}'.format(self.cliente) 
+    
