b00ad31fb7fc79e2e8ba4dbe5c42afecdfc6502b
raifran1 <raifranlucas@outlook.com>
2020-02-02 15:47:02 -0300

    atualizando caminho static

--- formare/settings.py
+++ formare/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = config('STATIC_ROOT')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    config('STATICFILES_DIRS')
 ]
